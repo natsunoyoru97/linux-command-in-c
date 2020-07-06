84aeab225a29b4bc031f662f052f576a00969c5c
natsunoyoru97 <honey970125@gmail.com>
2020-07-06 09:19:38 +0800

    add hello1.c

---
+++ video_game/hello1.c
@@ -0,0 +1,22 @@
+/* hello1.c
+ * purpose		show the minimal calls needed to use curses
+ * outline		initialize, draw stuff, wait for input, quit
+ */
+
+#include <stdio.h>
+#include <curses.h>
+
+int main()
+{
+	initscr();
+
+	clear();
+	move(10, 20);
+	addstr("Hello, world");
+	move (LINES - 1, 0);
+	refresh();
+	getch();
+	endwin();
+
+	return 0;
+}
